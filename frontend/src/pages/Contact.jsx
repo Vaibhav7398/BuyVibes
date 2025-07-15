ff3e2fd2a51a52e761774cbd6409c67e42eb9903
Vaibhav Srivastava <123542549+Vaibhav7398@users.noreply.github.com>
2025-07-15 11:46:53 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -12,10 +12,10 @@ const Contact = () => {
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
-          <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className='text-gray-500'>54709 Willms Station <br /> Suite 350, Washington, USA </p>
-          <p className='text-gray-500'>Tel: (415) 555-0132 <br /> Email: admin@forever.com</p>
-          <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
+          <p className='font-semibold text-xl text-gray-600'></p>
+          <p className='text-gray-500'> <br /> Delhi </p>
+          <p className='text-gray-500'>Tel: (415) 555-01XX <br /> Email: admin@buyvibes.com</p>
+          <p className='font-semibold text-xl text-gray-600'>Careers at BuyVibes</p>
           <p className='text-gray-600'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
